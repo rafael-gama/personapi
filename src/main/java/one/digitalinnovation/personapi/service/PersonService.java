d180d6dfae411fe5ab6992252a8ff603b8fde82f
rafael <rafaelantunesjf@gmail.com>
2021-09-11 22:10:31 -0300

    Developed list all method.

--- src/main/java/one/digitalinnovation/personapi/service/PersonService.java
+++ src/main/java/one/digitalinnovation/personapi/service/PersonService.java
@@ -9,6 +9,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestBody;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 public class PersonService {
 
@@ -32,5 +35,10 @@ public class PersonService {
     }
 
 
-
+    public List<PersonDTO> listAll() {
+        List<Person> allPeople = personRepository.findAll();
+        return allPeople.stream()
+                        .map(personMapper::toDTO)
+                        .collect(Collectors.toList());
+    }
 }
